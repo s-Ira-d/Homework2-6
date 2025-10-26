f90f6c540fbf627c9c5810c025a253284fe42856
s-Ira-d <20irinka12@gmail.com>
2025-10-26 14:50:57 +0200

    .

---
+++ dist/Homework2-6.d8897e08.js
@@ -0,0 +1,8 @@
+document.getElementById("result").innerHTML=`
+  <p>\u043F\u0440\u0438\u0432\u0456\u0442 \u0437 \u043C\u043E\u0434\u0443\u043B\u044F messages!</p>
+  <p>5 + 3 = 8</p>
+  <p>5 \xd7 3 = 15</p>
+  <p>5\xb2 = ${25}</p>
+  <p>\u0434\u043E \u0437\u0443\u0441\u0442\u0440\u0456\u0447\u0456!</p>
+`;
+//# sourceMappingURL=Homework2-6.d8897e08.js.map
